7068d6b309cbb7ad4d379f16f9709024825c8605
TooMuchPowers74 <62666700+TooMuchPowers74@users.noreply.github.com>
2020-12-16 12:07:40 -0500

    Update install-dhcp-server.sh

--- scripts/scriptlets/master/install-dhcp-server.sh
+++ scripts/scriptlets/master/install-dhcp-server.sh
@@ -8,7 +8,7 @@ sudo sed -i 's/INTERFACESv4=""/INTERFACESv4="eth0"/g' /etc/default/isc-dhcp-serv
 # sudo sed -i 's/INTERFACESv6=""/INTERFACESv6="eth0"/g' /etc/default/isc-dhcp-server > /dev/null
 
 # Append required dhcp config to system config
-cat ~/Raspberry-Pi-Kubernetes-Cluster-master/scripts/scriptlets/master/dhcpd.conf | sudo tee -a /etc/dhcp/dhcpd.conf > /dev/null
+cat ~/K8S-master/scripts/scriptlets/master/dhcpd.conf | sudo tee -a /etc/dhcp/dhcpd.conf > /dev/null
 
 echo -e "\nStarting DHCP Server\n"
 
